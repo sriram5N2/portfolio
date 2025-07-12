55498cba388dc7de4b2e12876ecc17fa47642a9d
harshavardhan314 <hharshavardhan974@gmail.com>
2025-07-12 19:31:36 +0530

    about

--- script.js
+++ script.js
@@ -4,3 +4,17 @@ const navbar = document.querySelector('.navbar');
 menuIcon.addEventListener('click', () => {
   navbar.classList.toggle('active');
 });
+
+  function toggleReadMore() {
+    const content = document.getElementById('readMoreContent');
+    const btn = document.querySelector('.readbtn a');
+
+    if (content.style.display === 'block') {
+      content.style.display = 'none';
+      btn.textContent = 'Read More';
+    } else {
+      content.style.display = 'block';
+      btn.textContent = 'Read Less';
+    }
+  }
+
